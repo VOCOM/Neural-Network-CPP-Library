adc9e73756e1a02c508ede36fd540e1dfad507a4
Muhd Syamim <Syazam33@gmail.com>
2023-09-20 23:03:34 +0800

    Update main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Muhammad Syamim (Syazam33@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-09-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello World!\n";
+}
